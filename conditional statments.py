d39b0e66669b273d4f15f68df535acd03b32d4cb
eesa <eesayousufyousuf@gmail.com>
2025-04-05 11:58:13 +0530

    conditional

---
+++ conditional statments.py
@@ -0,0 +1,28 @@
+# conditional statements in python
+# if_elif_else(syntax)
+"""if(condition):
+    statement1
+elif(conditin):
+    statement2
+else:
+    statementN   """    
+# Traffic lights code
+"""light=input("light:")
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("you can go")
+else:
+    print("light is broken")  """
+    # Grade of students
+marks=int(input(" marks:"))
+if(marks>=90):
+    print("A")
+elif(marks>=80 and marks<90):
+    print("B")
+elif(marks>=70 and marks<80):
+    print("C")
+else:
+    print("D")               
