7ef6c3d3c941709ed951314a5339156a33fe8a2c
eesa <eesayousufyousuf@gmail.com>
2025-05-10 09:39:26 +0530

    functions

---
+++ fun_day46.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print("good day,"+name)
+greet("eesa have a good day") 
+# 
+def mysum(num1,num2):
+    return num1 + num2
+s=mysum(4,5)
+print(s)  
---
+++ functions.py
@@ -0,0 +1,7 @@
+# functions in python
+marks=[45,60,70,90,35]
+percentage1=(sum(marks)/400)*100
+
+marks2=[75,78,88,78]
+percentage2=(sum(marks2)/400)*100
+print(percentage1,percentage2)
