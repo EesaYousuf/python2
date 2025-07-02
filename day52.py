df7b8cbcb82c4381c1cec2e1d92b7a496f8c12c5
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-02 09:37:04 +0530

    try7

--- day52.py
+++ day52.py
@@ -62,4 +62,22 @@ nickname = input("Choose your nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
+# Listen to server
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("❌ An error occurred!")
+            client.close()
+            break
 
+# Send messages
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('utf-8'))
