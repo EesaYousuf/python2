f0dbf510dec0555ede675eca294690f688655f78
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-02 09:33:58 +0530

    try1

---
+++ day52.py
@@ -0,0 +1,13 @@
+import socket
+import threading
+
+# Server settings
+HOST = '127.0.0.1'  # localhost
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+nicknames = []
