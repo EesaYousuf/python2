6e94dd4d4a50adc0b8b186015e51d036088f9ca5
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-05 14:21:41 +0530

    try2

--- day54.py
+++ day54.py
@@ -2,3 +2,19 @@
 
 import pefile
 import os
+def extract_features(file_path):
+    try:
+        pe = pefile.PE(file_path)
+        features = {
+            'SizeOfCode': pe.OPTIONAL_HEADER.SizeOfCode,
+            'SizeOfInitializedData': pe.OPTIONAL_HEADER.SizeOfInitializedData,
+            'SizeOfUninitializedData': pe.OPTIONAL_HEADER.SizeOfUninitializedData,
+            'AddressOfEntryPoint': pe.OPTIONAL_HEADER.AddressOfEntryPoint,
+            'BaseOfCode': pe.OPTIONAL_HEADER.BaseOfCode,
+            'ImageBase': pe.OPTIONAL_HEADER.ImageBase,
+            'Subsystem': pe.OPTIONAL_HEADER.Subsystem,
+            'DllCharacteristics': pe.OPTIONAL_HEADER.DllCharacteristics,
+            'SizeOfImage': pe.OPTIONAL_HEADER.SizeOfImage,
+            'NumberOfSections': len(pe.sections)
+        }
+        return features
