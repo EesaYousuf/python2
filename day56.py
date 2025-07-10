0979e9c1c4abc96c7d8aa6da99e5e81f639d69df
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-10 21:46:19 +0530

    try3

--- day56.py
+++ day56.py
@@ -4,3 +4,6 @@ from abc import ABC, abstractmethod
 # Abstract base class
 class Employee(ABC):
     company_name = "TechCorp"  # Class variable
+    def __init__(self, name, salary):
+        self._name = name         # Protected variable
+        self.__salary = salary    # Private variable
